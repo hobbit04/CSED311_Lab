b1e9d172fce6b1f62899aa980a9a955e16d4a9c4
hobbit04 <prlmejake9@gmail.com>
2025-02-27 15:51:35 +0900

    finished lab 1.a

---
+++ Lab1/alu/alu.v
@@ -0,0 +1,67 @@
+`include "alu_func.v"
+
+
+module ALU #(parameter data_width = 16) (
+	input [data_width - 1 : 0] A, 
+	input [data_width - 1 : 0] B, 
+	input [3 : 0] FuncCode,
+       	output reg [data_width - 1: 0] C,
+       	output reg OverflowFlag);
+// Do not use delay in your implementation.
+
+// You can declare any variables as needed.
+/*
+	YOUR VARIABLE DECLARATION...
+*/
+
+initial begin
+	C = 0;
+	OverflowFlag = 0;
+end   	
+
+// TODO: You should implement the functionality of ALU!
+// (HINT: Use 'always @(...) begin ... end')
+/*
+	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
+*/
+
+// todo: find correct operations for each function
+// todo: how to handle overflow
+// todo: make each module 
+
+always @(FuncCode or A or B) 
+	begin
+		OverflowFlag = 0;
+		case(FuncCode)
+		`FUNC_ADD : begin
+			C = A + B;
+			if ((A[data_width - 1] == B[data_width - 1]) && (C[data_width - 1] != A[data_width - 1]))
+				OverflowFlag = 1;
+		end
+		`FUNC_SUB : begin
+			C = A - B;
+			if ((A[data_width - 1] == B[data_width - 1]) && (C[data_width - 1] != A[data_width - 1]))
+				OverflowFlag = 1;
+		end
+		
+		`FUNC_AND : C = A & B;
+		`FUNC_OR : C = A | B;
+		`FUNC_NAND : C = ~(A & B);
+		`FUNC_NOR : C = ~(A | B);
+		`FUNC_XOR : C = A ^ B;
+		`FUNC_XNOR : C = ~(A ^ B);
+
+		`FUNC_ID : C = A;
+		`FUNC_NOT : C = ~A;
+		`FUNC_LLS : C = A << 1'b1;
+		`FUNC_LRS : C = A >> 1'b1;
+		`FUNC_ALS : C = A <<< 1'b1;
+		`FUNC_ARS : C = A >>> 1'b1;
+		`FUNC_TCP : C = ~A + 1'b1;
+		`FUNC_ZERO : C = 0;
+		default: C = 1'b0;
+		endcase
+	end
+
+endmodule
+
---
+++ Lab1/alu/obj_dir/Valu___024root__DepSet_h990b6f7d__0.cpp
@@ -0,0 +1,238 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Valu.h for the primary calling header
+
+#include "Valu__pch.h"
+#include "Valu___024root.h"
+
+void Valu___024root___ico_sequent__TOP__0(Valu___024root* vlSelf);
+
+void Valu___024root___eval_ico(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_ico\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Body
+    if ((1ULL & vlSelfRef.__VicoTriggered.word(0U))) {
+        Valu___024root___ico_sequent__TOP__0(vlSelf);
+    }
+}
+
+VL_INLINE_OPT void Valu___024root___ico_sequent__TOP__0(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___ico_sequent__TOP__0\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Body
+    vlSelfRef.OverflowFlag = 0U;
+    if ((8U & (IData)(vlSelfRef.FuncCode))) {
+        vlSelfRef.C = (0xffffU & ((4U & (IData)(vlSelfRef.FuncCode))
+                                   ? ((2U & (IData)(vlSelfRef.FuncCode))
+                                       ? ((1U & (IData)(vlSelfRef.FuncCode))
+                                           ? 0U : ((IData)(1U) 
+                                                   + 
+                                                   (~ (IData)(vlSelfRef.A))))
+                                       : ((1U & (IData)(vlSelfRef.FuncCode))
+                                           ? ((IData)(vlSelfRef.A) 
+                                              >> 1U)
+                                           : ((IData)(vlSelfRef.A) 
+                                              << 1U)))
+                                   : ((2U & (IData)(vlSelfRef.FuncCode))
+                                       ? ((1U & (IData)(vlSelfRef.FuncCode))
+                                           ? ((IData)(vlSelfRef.A) 
+                                              >> 1U)
+                                           : ((IData)(vlSelfRef.A) 
+                                              << 1U))
+                                       : ((1U & (IData)(vlSelfRef.FuncCode))
+                                           ? (~ ((IData)(vlSelfRef.A) 
+                                                 ^ (IData)(vlSelfRef.B)))
+                                           : ((IData)(vlSelfRef.A) 
+                                              ^ (IData)(vlSelfRef.B))))));
+    } else if ((4U & (IData)(vlSelfRef.FuncCode))) {
+        vlSelfRef.C = (0xffffU & ((2U & (IData)(vlSelfRef.FuncCode))
+                                   ? ((1U & (IData)(vlSelfRef.FuncCode))
+                                       ? (~ ((IData)(vlSelfRef.A) 
+                                             | (IData)(vlSelfRef.B)))
+                                       : (~ ((IData)(vlSelfRef.A) 
+                                             & (IData)(vlSelfRef.B))))
+                                   : ((1U & (IData)(vlSelfRef.FuncCode))
+                                       ? ((IData)(vlSelfRef.A) 
+                                          | (IData)(vlSelfRef.B))
+                                       : ((IData)(vlSelfRef.A) 
+                                          & (IData)(vlSelfRef.B)))));
+    } else if ((2U & (IData)(vlSelfRef.FuncCode))) {
+        vlSelfRef.C = (0xffffU & ((1U & (IData)(vlSelfRef.FuncCode))
+                                   ? (~ (IData)(vlSelfRef.A))
+                                   : (IData)(vlSelfRef.A)));
+    } else if ((1U & (IData)(vlSelfRef.FuncCode))) {
+        vlSelfRef.C = (0xffffU & ((IData)(vlSelfRef.A) 
+                                  - (IData)(vlSelfRef.B)));
+        if ((((1U & ((IData)(vlSelfRef.A) >> 0xfU)) 
+              == (1U & ((IData)(vlSelfRef.B) >> 0xfU))) 
+             & ((1U & ((IData)(vlSelfRef.C) >> 0xfU)) 
+                != (1U & ((IData)(vlSelfRef.A) >> 0xfU))))) {
+            vlSelfRef.OverflowFlag = 1U;
+        }
+    } else {
+        vlSelfRef.C = (0xffffU & ((IData)(vlSelfRef.A) 
+                                  + (IData)(vlSelfRef.B)));
+        if ((((1U & ((IData)(vlSelfRef.A) >> 0xfU)) 
+              == (1U & ((IData)(vlSelfRef.B) >> 0xfU))) 
+             & ((1U & ((IData)(vlSelfRef.C) >> 0xfU)) 
+                != (1U & ((IData)(vlSelfRef.A) >> 0xfU))))) {
+            vlSelfRef.OverflowFlag = 1U;
+        }
+    }
+}
+
+void Valu___024root___eval_triggers__ico(Valu___024root* vlSelf);
+
+bool Valu___024root___eval_phase__ico(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_phase__ico\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Init
+    CData/*0:0*/ __VicoExecute;
+    // Body
+    Valu___024root___eval_triggers__ico(vlSelf);
+    __VicoExecute = vlSelfRef.__VicoTriggered.any();
+    if (__VicoExecute) {
+        Valu___024root___eval_ico(vlSelf);
+    }
+    return (__VicoExecute);
+}
+
+void Valu___024root___eval_act(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_act\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+}
+
+void Valu___024root___eval_nba(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_nba\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+}
+
+void Valu___024root___eval_triggers__act(Valu___024root* vlSelf);
+
+bool Valu___024root___eval_phase__act(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_phase__act\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Init
+    VlTriggerVec<0> __VpreTriggered;
+    CData/*0:0*/ __VactExecute;
+    // Body
+    Valu___024root___eval_triggers__act(vlSelf);
+    __VactExecute = vlSelfRef.__VactTriggered.any();
+    if (__VactExecute) {
+        __VpreTriggered.andNot(vlSelfRef.__VactTriggered, vlSelfRef.__VnbaTriggered);
+        vlSelfRef.__VnbaTriggered.thisOr(vlSelfRef.__VactTriggered);
+        Valu___024root___eval_act(vlSelf);
+    }
+    return (__VactExecute);
+}
+
+bool Valu___024root___eval_phase__nba(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_phase__nba\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Init
+    CData/*0:0*/ __VnbaExecute;
+    // Body
+    __VnbaExecute = vlSelfRef.__VnbaTriggered.any();
+    if (__VnbaExecute) {
+        Valu___024root___eval_nba(vlSelf);
+        vlSelfRef.__VnbaTriggered.clear();
+    }
+    return (__VnbaExecute);
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Valu___024root___dump_triggers__ico(Valu___024root* vlSelf);
+#endif  // VL_DEBUG
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Valu___024root___dump_triggers__nba(Valu___024root* vlSelf);
+#endif  // VL_DEBUG
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Valu___024root___dump_triggers__act(Valu___024root* vlSelf);
+#endif  // VL_DEBUG
+
+void Valu___024root___eval(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Init
+    IData/*31:0*/ __VicoIterCount;
+    CData/*0:0*/ __VicoContinue;
+    IData/*31:0*/ __VnbaIterCount;
+    CData/*0:0*/ __VnbaContinue;
+    // Body
+    __VicoIterCount = 0U;
+    vlSelfRef.__VicoFirstIteration = 1U;
+    __VicoContinue = 1U;
+    while (__VicoContinue) {
+        if (VL_UNLIKELY((0x64U < __VicoIterCount))) {
+#ifdef VL_DEBUG
+            Valu___024root___dump_triggers__ico(vlSelf);
+#endif
+            VL_FATAL_MT("alu.v", 4, "", "Input combinational region did not converge.");
+        }
+        __VicoIterCount = ((IData)(1U) + __VicoIterCount);
+        __VicoContinue = 0U;
+        if (Valu___024root___eval_phase__ico(vlSelf)) {
+            __VicoContinue = 1U;
+        }
+        vlSelfRef.__VicoFirstIteration = 0U;
+    }
+    __VnbaIterCount = 0U;
+    __VnbaContinue = 1U;
+    while (__VnbaContinue) {
+        if (VL_UNLIKELY((0x64U < __VnbaIterCount))) {
+#ifdef VL_DEBUG
+            Valu___024root___dump_triggers__nba(vlSelf);
+#endif
+            VL_FATAL_MT("alu.v", 4, "", "NBA region did not converge.");
+        }
+        __VnbaIterCount = ((IData)(1U) + __VnbaIterCount);
+        __VnbaContinue = 0U;
+        vlSelfRef.__VactIterCount = 0U;
+        vlSelfRef.__VactContinue = 1U;
+        while (vlSelfRef.__VactContinue) {
+            if (VL_UNLIKELY((0x64U < vlSelfRef.__VactIterCount))) {
+#ifdef VL_DEBUG
+                Valu___024root___dump_triggers__act(vlSelf);
+#endif
+                VL_FATAL_MT("alu.v", 4, "", "Active region did not converge.");
+            }
+            vlSelfRef.__VactIterCount = ((IData)(1U) 
+                                         + vlSelfRef.__VactIterCount);
+            vlSelfRef.__VactContinue = 0U;
+            if (Valu___024root___eval_phase__act(vlSelf)) {
+                vlSelfRef.__VactContinue = 1U;
+            }
+        }
+        if (Valu___024root___eval_phase__nba(vlSelf)) {
+            __VnbaContinue = 1U;
+        }
+    }
+}
+
+#ifdef VL_DEBUG
+void Valu___024root___eval_debug_assertions(Valu___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Valu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Valu___024root___eval_debug_assertions\n"); );
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Body
+    if (VL_UNLIKELY((vlSelfRef.FuncCode & 0xf0U))) {
+        Verilated::overWidthError("FuncCode");}
+}
+#endif  // VL_DEBUG
