870f452afb081411d784a30f7934a28c99b028b8
Daniel Jiesung Jung <daniel72@postech.ac.kr>
2025-03-04 20:38:35 +0900

    two modules

--- Lab1/vending_machine/check_time_and_coin.v
+++ Lab1/vending_machine/check_time_and_coin.v
@@ -13,24 +13,33 @@ module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,wait_time,o_re
 	// initiate values
 	initial begin
 		// TODO: initiate values
+		wait_time = `kWaitTime;
+		o_return_coin = 0;
 	end
 
 
 	// update coin return time
 	always @(i_input_coin, i_select_item) begin
 		// TODO: update coin return time
+		wait_time = `kWaitTime;  // 입력이 들어올 때마다, 대기 시간을 초기화 함.
 	end
 
 	always @(*) begin
 		// TODO: o_return_coin
+		o_return_coin = i_input_coin;  // 일단 테스트는 통과할 듯..? 구매하면서 얘도 확인하는 테스트가 없음
+		
 	end
 
 	always @(posedge clk ) begin
 		if (!reset_n) begin
 		// TODO: reset all states.
+			wait_time <= `kWaitTime;
+			o_return_coin <= 0;
 		end
 		else begin
 		// TODO: update all states.
+			wait_time <= wait_time - 1; 
+			o_return_coin <= i_input_coin;
 		end
 	end
 endmodule 
