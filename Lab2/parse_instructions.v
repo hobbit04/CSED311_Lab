d216fb94120e6d9181a6a3ed2ce75c250a1edd82
Daniel Jiesung Jung <daniel72@postech.ac.kr>
2025-03-22 21:41:49 +0900

    changed parse instruction logic

--- Lab2/parse_instructions.v
+++ Lab2/parse_instructions.v
@@ -1,15 +1,17 @@
 module parse_instructions(
     input [31:0] instruction,
     output [6:0] opcode,
+    output [2:0] funct3,
+    output [6:0] funct7,
     output [4:0] rs1,
     output [4:0] rs2,
-    output [4:0] rd,
-    output [10:0] alu_ctrl_input
+    output [4:0] rd
     );
 
     assign opcode = instruction[6:0];
+    assign funct3 = instruction[14:12];
+    assign funct7 = instruction[31:25];
     assign rs1 = instruction[19:15];
     assign rs2 = instruction[24:20];
     assign rd = instruction[11:7];
-    assign alu_ctrl_input = {instruction[30], instruction[14:12], instruction[6:0]};
 endmodule
