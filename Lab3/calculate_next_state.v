9563d73693e1f745331171ee728f43f78f3ffaea
hobbit04 <prlmejake9@gmail.com>
2025-04-13 20:26:17 +0900

    control unit start

---
+++ Lab3/calculate_next_state.v
@@ -0,0 +1,11 @@
+module calculate_next_state(
+    input [3:0] added_state,  // state + 1
+    input [6:0] opcode,
+    input [3:0] AddrCtrl,
+    output reg [3:0] next_state
+);
+    
+    always @(*) begin
+        
+    end
+endmodule
