0e95b848fc3ecbc4b8170f0d3580d63cdd948a11
hobbit04 <prlmejake9@gmail.com>
2025-04-27 21:46:02 +0900

    forwarding plan

--- Lab4-1/StallDetection.v
+++ Lab4-1/StallDetection.v
@@ -3,8 +3,8 @@ module StallDetection(
     input [4:0] ID_rs1,
     input [4:0] ID_rs2,
     input [6:0] ID_opcode,
-    input [4:0] EX_rd,
-    input EX_reg_write,
+    // input [4:0] EX_rd,
+    // input EX_reg_write,
     input [4:0] MEM_rd,
     input MEM_reg_write,
     output reg is_stall
@@ -14,7 +14,8 @@ module StallDetection(
     wire rs1_conditions;
 
     wire use_rs1, use_rs2;
-    wire stall_by_ex, stall_by_mem;
+    // wire stall_by_ex, stall_by_mem;
+    wire stall_by_mem;
 
 
     assign rs1_and_rs2_conditions = (ID_opcode == `ARITHMETIC) || (ID_opcode == `STORE);
@@ -23,8 +24,8 @@ module StallDetection(
     assign use_rs1 = (rs1_and_rs2_conditions || rs1_conditions) && (ID_rs1 != 5'b0);
     assign use_rs2 = rs1_and_rs2_conditions && (ID_rs2 != 5'b0);
 
-    assign stall_by_ex = ((ID_rs1 == EX_rd) && use_rs1 && EX_reg_write) ||
-                             ((ID_rs2 == EX_rd) && use_rs2 && EX_reg_write);
+    // assign stall_by_ex = ((ID_rs1 == EX_rd) && use_rs1 && EX_reg_write) ||
+    //                          ((ID_rs2 == EX_rd) && use_rs2 && EX_reg_write);
     assign stall_by_mem = ((ID_rs1 == MEM_rd) && use_rs1 && MEM_reg_write) ||
                               ((ID_rs2 == MEM_rd) && use_rs2 && MEM_reg_write);
 
