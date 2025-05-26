d259c2687729b60d9034632eab8ebdbd0715f71e
hobbit04 <prlmejake9@gmail.com>
2025-05-27 02:41:18 +0900

    Direct mapped finished

--- Lab5/DirMapCache.v
+++ Lab5/DirMapCache.v
@@ -45,10 +45,12 @@ module DirMapCache #(parameter LINE_SIZE = 16,
   reg data_mem_is_input_valid, data_mem_read, data_mem_write;
   reg [31:0] data_mem_addr;
   reg [LINE_SIZE * 8 - 1:0] data_mem_din;
+
+  reg reg2cache, mem2cache, clearCache;  // cache control signals
  
   integer i, j;
 
-  assign is_ready = (state == IDLE); 
+  assign is_ready = (next_state == IDLE); 
   assign is_output_valid = (state == COMPARE_TAG && is_hit);
   assign dout = data_bank[index][offset[3:2]*32 +: 32];  
   assign is_hit = (valid_bit[index] && tag_bank[index] == tag);
@@ -59,7 +61,7 @@ module DirMapCache #(parameter LINE_SIZE = 16,
     .clk(clk),
 
     .is_input_valid(data_mem_is_input_valid),
-    .addr(data_mem_addr),        // NOTE: address must be shifted by CLOG2(LINE_SIZE)
+    .addr(data_mem_addr >> 4),        // NOTE: address must be shifted by CLOG2(LINE_SIZE)
     .mem_read(data_mem_read),
     .mem_write(data_mem_write),
     .din(data_mem_din),
@@ -72,26 +74,56 @@ module DirMapCache #(parameter LINE_SIZE = 16,
   );
 
   always @(*) begin
+    // Data memory control signals
     data_mem_is_input_valid = 0;
     data_mem_addr = 0;
     data_mem_read = 0;
     data_mem_write = 0;
     data_mem_din = 0;
+
+    // Cache control signals
+    reg2cache = 0;
+    mem2cache = 0;
+    clearCache = 0;
+
     case(state)
       IDLE: begin
       end
       COMPARE_TAG: begin
+        if(is_hit && mem_write) begin
+          reg2cache = 1;  // data mem -> cache
+        end
+        else begin
+          if(next_state == ALLOCATE) begin
+            data_mem_is_input_valid = 1;
+            data_mem_addr = addr;  // allocate the block
+            data_mem_read = 1;
+          end
+          else if(next_state == WRITEBACK) begin
+            data_mem_is_input_valid = 1;
+            data_mem_addr = {tag_bank[index], index, 4'b0000}; // write back the dirty block
+            data_mem_din = data_bank[index];
+            data_mem_write = 1;
+          end
+        end
       end
       WRITEBACK: begin
-        data_mem_is_input_valid = 1;
-        data_mem_write = 1;
-        data_mem_addr = {tag_bank[index], index, 4'b0000} >> 4; // write back the dirty block
-        data_mem_din = data_bank[index];
+        if(next_state == ALLOCATE) begin
+          data_mem_is_input_valid = 1;
+          data_mem_read = 1;
+          data_mem_addr = addr; // write back the dirty block
+          data_mem_din = data_bank[index];
+        end
+
+        
+        if(is_data_mem_ready) begin
+          clearCache = 1;
+        end
       end
       ALLOCATE: begin
-        data_mem_is_input_valid = 1;
-        data_mem_addr = addr >> 4;
-        data_mem_read = 1;
+        if(is_data_mem_ready) begin
+          mem2cache = 1;  // data mem -> cache
+        end
       end
     endcase
   end
@@ -141,7 +173,7 @@ module DirMapCache #(parameter LINE_SIZE = 16,
         end
       end
       ALLOCATE: begin
-        if(is_data_mem_output_valid) begin  // 얘를 is_data_mem_ready로 바꾸면 8번은 맞고 13번은 틀림.. 현재는 8번이 틀리고 13번이 맞는 상태 -> 상태 전이와 타이밍 문제인 것 같음
+        if(is_data_mem_ready) begin  // 얘를 is_data_mem_ready로 바꾸면 8번은 맞고 13번은 틀림.. 현재는 8번이 틀리고 13번이 맞는 상태 -> 상태 전이와 타이밍 문제인 것 같음
           next_state = COMPARE_TAG;
         end
         else begin
@@ -169,26 +201,25 @@ module DirMapCache #(parameter LINE_SIZE = 16,
       end
     end
     else begin
-      case(state)
-        IDLE: begin
-        end
-        COMPARE_TAG: begin
-          if(is_hit && mem_write) begin
-            data_bank[index][ offset[3:2]*32 +: 32 ] <= din;
-            dirty_bit[index] <= 1;  // if miss-write, set dirty bit
-          end         
-        end
-        WRITEBACK: begin
-        end
-        ALLOCATE: begin
-          if(next_state == COMPARE_TAG) begin
-            data_bank[index] <= data_mem_dout;
-            tag_bank[index] <= tag;
-            valid_bit[index] <= 1;
-            dirty_bit[index] <= 0;
-          end
-        end
-      endcase
+      // 캐시는 언제 업데이트가 되는가?
+      // 1. din을 캐시에 저장하는 경우 -> 데이터 뱅크와 dirty_bit 업데이트 : by reg2cache
+      // 2. 데이터 메모리의 dout을 캐시에 저장하는 경우 -> cache miss가 났던 것이므로, Tag, Data, Valid 업데이트 : by mem2cache
+      if(reg2cache) begin
+        data_bank[index][ offset[3:2]*32 +: 32 ] <= din;
+        dirty_bit[index] <= 1;  
+      end
+      if(mem2cache) begin
+        data_bank[index] <= data_mem_dout;
+        tag_bank[index] <= tag;
+        valid_bit[index] <= 1;
+        dirty_bit[index] <= 0;  
+      end
+      if(clearCache) begin
+        data_bank[index] <= 0;
+        tag_bank[index] <= 0;
+        valid_bit[index] <= 0; 
+        dirty_bit[index] <= 0;
+      end
     end
   end
 endmodule
